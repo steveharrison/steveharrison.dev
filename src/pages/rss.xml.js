75c69adda08a4abd58e58dba4b064ba8662eaa71
Steve Harrison <steve@steveharrison.dev>
2026-05-07 15:42:05 +1000

    Fix RSS feed.

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,16 +1,42 @@
 import rss from '@astrojs/rss';
-import { getCollection } from 'astro:content';
+import { getCollection, render } from 'astro:content';
+import { experimental_AstroContainer as AstroContainer } from 'astro/container';
+import { loadRenderers } from 'astro:container';
+import { getContainerRenderer as getMDXRenderer } from '@astrojs/mdx';
+import sanitizeHtml from 'sanitize-html';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
 
 export async function GET(context) {
 	const posts = await getCollection('blog');
+	const renderers = await loadRenderers([getMDXRenderer()]);
+	const container = await AstroContainer.create({ renderers });
+
+	const items = await Promise.all(
+		posts.map(async (post) => {
+			const { Content } = await render(post);
+			const html = await container.renderToString(Content);
+			const content = sanitizeHtml(html, {
+				allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img', 'figure', 'figcaption']),
+				allowedAttributes: {
+					...sanitizeHtml.defaults.allowedAttributes,
+					'*': ['class', 'id', 'style'],
+					img: ['src', 'alt', 'title', 'width', 'height', 'loading', 'srcset', 'sizes'],
+					a: ['href', 'name', 'target', 'rel', 'title'],
+				},
+			});
+
+			return {
+				...post.data,
+				link: `/${post.id}/`,
+				content,
+			};
+		}),
+	);
+
 	return rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/${post.id}/`,
-		})),
+		items,
 	});
 }
